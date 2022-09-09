1c1a913ddea0e02d53f2a98b4c0da1403161fc49
aabyodj <aabyodj@gmail.com>
2022-09-09 03:23:06 +0300

    Don't use null to determine unset fields

--- system/Bootstrap.php
+++ system/Bootstrap.php
@@ -5,29 +5,29 @@ require_once('RepositoryJson.php');
 require_once('ServiceImpl.php');
 
 class Bootstrap {
-	private static ?Bootstrap $INSTANCE = null;
+	private static Bootstrap $INSTANCE;
 	
-	private ?Repository $repository = null;
-	private ?Service $service = null;
+	private Repository $repository;
+	private Service $service;
 	
 	private function __construct() {}
 	
 	public static function getInstance(): Bootstrap {
-		if (null == self::$INSTANCE) {
+		if (!isset(self::$INSTANCE)) {
 			self::$INSTANCE = new Bootstrap();
 		}
 		return self::$INSTANCE;
 	}
 	
 	public function getRepository(): Repository {
-		if (null == $this->repository) {
+		if (!isset($this->repository)) {
 			$this->repository = new RepositoryJson(JSON_REPO_DIR);
 		}
 		return $this->repository;
 	}
 	
 	public function getService(): Service {
-		if (null == $this->service) {
+		if (!isset($this->service)) {
 			$this->service = new ServiceImpl($this->getRepository());
 		}
 		return $this->service;
